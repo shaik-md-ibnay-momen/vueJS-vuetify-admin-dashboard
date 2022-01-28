90d19e25e6ec704b447c18e31ca8adbff1447d59
Shaik Md. Ibnay Momen <shaikmohammad864@gmail.com>
2022-01-29 00:47:55 +0600

    init

---
+++ todo3/src/App.vue
@@ -0,0 +1,17 @@
+<template>
+<v-app id="inspire" > 
+  <nav-drawer/>
+  <appbar/>
+
+</v-app>
+</template>
+
+<script>
+
+import navDrawer from './components/navDrawer.vue'
+import appbar from './components/appbar.vue'
+
+  export default {
+    components: { navDrawer , appbar},
+  }
+</script>
---
+++ todo3/src/components/appbar.vue
@@ -0,0 +1,65 @@
+<template>
+    <v-card class="overflow-hidden">
+    <v-app-bar
+      app
+      absolute
+      color="#e21d6f"
+      dark
+      shrink-on-scroll
+      prominent
+      src="https://i.imgur.com/Iko3cBX.jpeg"
+      height= '100px'
+      fade-img-on-scroll
+      scroll-target="#scrolling-techniques-3"
+    >
+
+      <template v-slot:img="{ props }">
+        <v-img
+          v-bind="props"
+          gradient="to top right, rgba(226, 29, 111, 1), rgba(25,32,72,.7)"
+        ></v-img>
+      </template>
+
+      <v-app-bar-title
+       
+      > <h2>Bkash</h2>  </v-app-bar-title>
+      <v-spacer></v-spacer>
+         <v-app-bar-title
+       
+      > <h2 >dotkom</h2>  </v-app-bar-title>
+       <v-spacer></v-spacer>
+      <v-btn icon>
+        <v-icon>mdi-magnify</v-icon>
+      </v-btn>
+
+      <v-btn icon>
+        <v-icon>mdi-heart</v-icon>
+      </v-btn>
+
+      <v-btn icon>
+        <v-icon>mdi-dots-vertical</v-icon>
+      </v-btn>
+
+      <template v-slot:extension>
+        <v-tabs align-with-title>
+          <v-tab>Tab 1</v-tab>
+          <v-tab>Tab 2</v-tab>
+          <v-tab>Tab 3</v-tab>
+        </v-tabs>
+      </template>
+    </v-app-bar>
+    <v-sheet
+      id="scrolling-techniques-3"
+      class="overflow-y-auto"
+      max-height="600"
+    >
+      <v-container style="height: 1000px;"></v-container>
+    </v-sheet>
+  </v-card>
+</template>
+
+<script>
+export default {
+ 
+}
+</script>
